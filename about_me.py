7e29e453707d1c3934b9c1b1c78ffa043803c2e7
HeaJin Hong <heajin.hong@yale.edu>
2025-10-14 11:45:50 -0400

    adding about_me.py

--- about_me.py
+++ about_me.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
-print('My name: HeaJin')
+name = "HeaJin"
+print(f"My name: {name}")
+print('My favorite color: Lavender')
+hobby = "video games"
+print("My favorite activity:", hobby)
+animal = "cat"
+animal_name = "(Kong)"
+print("My favorite animal:", animal, animal_name)
+
+import sys
+
+name = sys.argv[1]
+favorite_color = sys.argv[2]
+favorite_animal = sys.argv[3]
+print("My name is", name, "my favorite color is", favorite_color, 'and', 'my favorite animal is', favorite_animal)
+
