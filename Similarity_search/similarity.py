c960847dfa6a439fe7b60f6ff3132e4de7e04abd
HeaJin Hong <heajin.hong@yale.edu>
2025-10-23 20:20:38 -0400

    adding similarity_search workshop

---
+++ Similarity_search/similarity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+
+field_names = ['qseqid','sseqid','percid','alen','mismat','gaps','q_start','q_end','s_start','s_end','evalue','bits']
+hits_list = []
+for hit_file in sys.argv[1:]:
+    with open(hit_file,"r") as seq_read:
+        for line in seq_read:
+            line = line.rstrip()
+            if line.startswith('#'):
+                continue
+            this_data = dict(zip(field_names, line.split('\t')))
+            #print(this_data)
+            this_data['file'] = hit_file
+            hits_list.append(this_data)
+            break
+#print(hits_list)
+for hit in hits_list:
+    print('\t'.join([hit[x] for x in ('file','percid','alen','evalue')]))
+
